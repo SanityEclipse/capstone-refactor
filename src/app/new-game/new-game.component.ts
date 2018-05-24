f5405c095c56a680ad06528cb70fa7d976cf5ad8
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-05-23 22:25:06 -0600

    added final bit if phaser text.

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -9,11 +9,23 @@ import * as Phaser from '../../../node_modules/phaser-ce/build/phaser.js';
 })
 
 export class NewGameComponent {
+
+    game   : Phaser.Game;
+    map    : Phaser.Tilemap;
+    layer  : Phaser.TileMapLayer;
+    player : Phaser.Sprite;
+    
   constructor() {
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {preload : this.preload, create: this.create})
+      this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+        preload : this.preload,
+        create  : this.create,
+        update  : this.update,
+        render  : this.render
+      }
+    )
   }
 
-  game: Phaser.Game;
+
 
   preload() {
     this.game.load.image('logo', '../../assets/images/items/blue-gem.png');
@@ -22,5 +34,13 @@ export class NewGameComponent {
   create() {
       var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
       logo.anchor.setTo(0.5, 0.5);
+  }
+
+  update() {
+
+  }
+
+  render() {
+
   }
 }
