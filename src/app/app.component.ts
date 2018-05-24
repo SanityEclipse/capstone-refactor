d5eefdaf12274813fa8bfb665d424c0bf0e1a6b0
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-05-23 21:22:57 -0600

    Added assets from original game ot project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as Phaser from '../../node_modules/phaser-ce/build/phaser.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {preload : this.preload, create: this.create})
+  }
+
+  game: Phaser.Game;
+
+  preload() {
+    this.game.load.image('logo', './assets/images/IMG_0134.jpg');
+  }
+
+  create() {
+      var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+      logo.anchor.setTo(0.5, 0.5);
+  }
+
+  ngOnInit() {
+  }
+
   title = 'app';
+
 }
