b857563a7bf3c7ee0e19e04510b3f1b20c5727e2
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-05-30 21:15:51 -0600

    Added and formatted header and footer.

--- src/app/core/footer.component.ts
+++ src/app/core/footer.component.ts
@@ -3,18 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector:"app-footer",
   template:`
-    <nav class="navbar fixed-bottom">
-      <span class="navbar-brand">Fixed bottom</span>
+    <nav class="navbar fixed-bottom justify-content-center">
+      <span class="navbar-brand">© 2016-2018 Thomas Rizzo</span>
     </nav>
   `,
   styles:[`
     .navbar {
       background: black;
       color: red;
+      font-family: 'Press Start 2P';
     }`
   ]
 })
 
-export class FooterComponent {
-
-}
+export class FooterComponent {}
--- src/app/core/navbar.component.ts
+++ src/app/core/navbar.component.ts
@@ -3,14 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector:"app-navbar",
   template:`
-    <nav class="navbar fixed-top">
-      <span class="navbar-brand mb-0 h1">Navbar</span>
+    <nav class="navbar fixed-top justify-content-center">
+      <span class="navbar-brand mb-0 h1">The Flame Templar Saga</span>
     </nav>
   `,
   styles:[`
     .navbar {
       background: black;
       color: red;
+      font-family: 'Press Start 2P'
     }
     `]
 })
