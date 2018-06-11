05ede8300a282a6e56e0eeac08bbdb94dd4bb966
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-06-10 23:15:39 -0600

    Score service working for both get and post

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, NgZone, OnInit, OnDestroy } from '@angular/core';
 
 import * as Phaser from '../../../node_modules/phaser-ce/build/phaser.js';
 
@@ -8,7 +8,6 @@ import { Menu }       from './game-states/menu.component';
 import { Directions } from './game-states/directions.component';
 import { Level1 }     from './game-states/level-1.component';
 import { Endgame }    from './game-states/endgame.component';
-
 import { ScoreService } from './score.service';
 
 import { Score } from './Score';
@@ -25,7 +24,8 @@ export class NewGameComponent implements OnInit {
     scores: Score[] = [];
 
   constructor(
-    private ScoreService : ScoreService
+    private scoreService : ScoreService,
+    private ngZone : NgZone
   ){
     this.game = new Phaser.Game(725, 525, Phaser.AUTO, 'content')
     this.game.state.add('Boot', Boot, false);
@@ -39,11 +39,26 @@ export class NewGameComponent implements OnInit {
 
   ngOnInit() {
     this.getScores();
+    window.my = window.my || {};
+    window.my.namespace = window.my.namespace || {};
+    window.my.namespace.addScore = this.addScore.bind(this);
   }
 
   getScores(): void {
-    this.ScoreService.getScores()
+    this.scoreService.getScores()
      .subscribe(scores => this.scores = scores);
+  }
+
+  addScore(id, name, game): void{
+    this.ngZone.run(() => this.privateFunc(id, name, game));
+  }
+
+  privateFunc(id:number, name:string, score:number): void {
+    console.log("adding scores!!!")
+    this.scoreService.createScores({id, name, score} as Score)
+      .subscribe(score => {
+        this.scores.push(score);
+    });
   }
 
   ngOnDestroy() {
--- src/app/new-game/score.service.ts
+++ src/app/new-game/score.service.ts
@@ -22,49 +22,16 @@ export class ScoreService {
   }
 
   getScores(): Observable<Score[]> {
-    console.log(Score);
     return this.http.get<Score[]>('api/scores')
-    // .pipe(
-    //   tap(scores => this.log(`fetched scores`)),
-    //   catchError(this.handleError('getScores', []))
-    // );
   }
 
-  // createScores(name, score) {
-  //
-  //   this.http.post('/highscores', { name: name,  score: score })
-  //       .toPromise()
-  //       .then()
-  //       .catch(this.handleError);
-  //   }
-
-  createScores(score:Score):Observable<Score> {
-    return this.http.post<Score>('./highscores', score, httpOptions)
+  createScores(score:Score): Observable<Score>{
+    return this.http.post<Score>('api/scores', score, httpOptions)
   }
-
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
       return Promise.reject(error.message || error);
   }
-
-//   private handleError<T> (operation = 'operation', result?: T) {
-//   return (error: any): Observable<T> => {
-//
-//     // TODO: send the error to remote logging infrastructure
-//     console.error(error); // log to console instead
-//
-//     // TODO: better job of transforming error for user consumption
-//     this.log(`${operation} failed: ${error.message}`);
-//
-//     // Let the app keep running by returning an empty result.
-//     return of(result as T);
-//   };
-// }
-//
-// /** Log a HeroService message with the MessageService */
-// private log(message: string) {
-//   this.messageService.add('HeroService: ' + message);
-// }
 
 }
