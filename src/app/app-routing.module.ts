8128d6a0c701b685ccc7dcd57d58d254e66079c9
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-06-27 09:44:52 -0600

    Pivot- electing instead for single view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: 'new-game',
-    loadChildren: './new-game/new-game.module#NewGameModule'
-    // data: {}
+    loadChildren: './new-game/new-game.module#NewGameModule',
+    data: {}
   },
-  {
-    path: 'tech-used',
-    loadChildren: './tech-used/tech-used.module#TechUsedModule'
-    // data: {}
-  },
-  // {
-  //   path: 'contact-me',
-  //   loadChildren: './tech-used/tech-used.module#ContactMeModule',
-  //   data: {}
-  // }
   {
     path: '',
     redirectTo: '',
