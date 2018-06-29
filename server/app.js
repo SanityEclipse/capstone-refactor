41e67a31d68f7dfd5353cbe689232e764d83c5ab
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-06-29 11:04:56 -0600

    added more package json commands

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // catch 404 and forward to error handler
