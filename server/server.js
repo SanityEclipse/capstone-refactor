614cf3e8397f404df0c96b387d2570a0ff96d884
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-07-09 21:32:32 -0600

    Added extra server scaffolding and files.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 'use strict'
 
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
-const http = require('http');
+const routes = require('./routes/index')
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const queries = require ('./db/queries');
 
 app.use(express.static('dist'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
